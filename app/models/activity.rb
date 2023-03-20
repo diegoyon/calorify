57bca339682cdd8aa65806073e3418775e3eef2a
Diego <diegoyon@outlook.com>
2023-03-20 10:48:32 -0600

    add validations

--- app/models/activity.rb
+++ app/models/activity.rb
@@ -1,3 +1,6 @@
 class Activity < ApplicationRecord
+  validates :calories, presence: true
+  validates :description, presence: true, length: { maximum: 200 }
+
   attribute :burned, :boolean, default: false
 end
