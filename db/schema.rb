d8d5f0ae145adf7dd2e4e174f8c76128b8cee54c
Diego <diegoyon@outlook.com>
2023-03-20 16:54:29 -0600

    change to postgres

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,16 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[7.0].define(version: 2023_03_20_173043) do
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "activities", force: :cascade do |t|
     t.integer "calories"
     t.boolean "burned"
     t.string "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer "user_id", null: false
+    t.bigint "user_id", null: false
     t.index ["user_id"], name: "index_activities_on_user_id"
   end
 
