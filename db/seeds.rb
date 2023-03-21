3f186ebdc3372d28a3588baa91715c03c2c3889e
Diego <diegoyon@outlook.com>
2023-03-21 15:47:56 -0600

    add seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+for i in 1..4 do #amount of users
+  random_name = "#{Faker::Name.first_name}#{i}"
+  user = User.create!(email: "#{random_name.downcase}@gmail.com", password: "#{random_name.downcase}@gmail.com")
+
+  (4.months.ago.to_date..Date.current).each do |date|
+    4.times do #activites per day
+      bool = Faker::Boolean.boolean
+      if bool
+        Activity.create!(date: date, calories: Faker::Number.within(range: 50..500), burned: true, description: Faker::Hobby.activity, user_id: user.id)
+      else
+        Activity.create!(date: date, calories: Faker::Number.within(range: 100..1000), burned: false, description: Faker::Food.dish, user_id: user.id)
+      end
+    end
+  end
+end
