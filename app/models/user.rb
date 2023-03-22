2fe6efaf7597f9d6f6cf8c3b74623795ad1833fe
Diego <diegoyon@outlook.com>
2023-03-22 15:50:48 -0600

    add token to user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,18 @@
 class User < ApplicationRecord
+  before_create :generate_token
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  
 
   has_many :activities
 
   attribute :activity_counter, default: 0
+
+  private
+
+    def generate_token
+      self.token = SecureRandom.urlsafe_base64
+    end
 end
