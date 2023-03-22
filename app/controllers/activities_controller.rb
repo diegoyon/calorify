ad2306305620a37259f5b45b366be49099df111f
Diego <diegoyon@outlook.com>
2023-03-21 22:55:30 -0600

    order by date instead of created_at

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -3,7 +3,7 @@ class ActivitiesController < ApplicationController
   load_and_authorize_resource
 
   def index
-    @activities = current_user.activities.order(created_at: :desc).page(params[:page])
+    @activities = current_user.activities.order(date: :desc).page(params[:page])
     search_activities
     filter_from
     filter_to
