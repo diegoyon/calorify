057d33464223179a948e3c6768f755f3e48e63ab
Diego <diegoyon@outlook.com>
2023-03-22 21:53:49 -0600

    add link to share with token

--- app/controllers/charts_controller.rb
+++ app/controllers/charts_controller.rb
@@ -1,15 +1,23 @@
 class ChartsController < ApplicationController
   skip_before_action :authenticate_user!, only: [:linechart]
   # /chart?user_id=4
+  # /chart?token=8Ynxp2XAwVoO3zbVe22itQ
   def linechart
-    if params[:user_id].present?
-      current_user = User.find_by(id: params[:user_id])
-      if current_user == nil
+    if params[:token].blank?
+      set_calories(current_user)
+    else
+      user = User.find_by(token: params[:token])
+      if user == nil
         redirect_to new_user_session_path, alert: 'Invalid URL'
         return
       end
+      set_calories(user)
     end
-    @calories_burned = current_user.activities.where("date >= ?", 30.days.ago.to_date).where(burned: true).group_by_day(:date).sum(:calories)
-    @calories_consumed = current_user.activities.where("date >= ?", 30.days.ago.to_date).where(burned: false).group_by_day(:date).sum(:calories)
   end
+
+  private
+    def set_calories(user)
+      @calories_burned = user.activities.where("date >= ?", 30.days.ago.to_date).where(burned: true).group_by_day(:date).sum(:calories)
+      @calories_consumed = user.activities.where("date >= ?", 30.days.ago.to_date).where(burned: false).group_by_day(:date).sum(:calories)
+    end
 end
