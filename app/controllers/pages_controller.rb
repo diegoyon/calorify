ba57a4e83511fc6a426972d4355b5b08710577c9
Diego <diegoyon@outlook.com>
2023-03-24 12:19:57 -0600

    add calorie_data actiont to skip authentication

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,5 @@
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: [:linechart]
+  skip_before_action :authenticate_user!, only: [:linechart, :calorie_data]
 
   def linechart
   end
