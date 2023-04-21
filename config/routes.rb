e97c41ea2bf9d750250de347b2857500be5c9394
Diego <diegoyon@outlook.com>
2023-04-21 10:56:04 -0600

    fix navbar and add home page

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   resources :activities, except: [:show]
-  root "activities#index"
+  root "home#index"
   get '/chart', to: 'pages#linechart'
   post '/share_chart', to: 'pages#share_chart'
   get '/profile', to: 'pages#profile'
